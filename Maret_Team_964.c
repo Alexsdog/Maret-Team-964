d5909408a1e5c03ce9eb5be3516792d05538d72e
Alexsdog <alexanderr2028@maret.org>
2026-03-20 18:38:50 -0400

    Comments Added

--- Maret_Team_964.c
+++ Maret_Team_964.c
@@ -1,5 +1,6 @@
 #include <kipr/wombat.h>
 
+//Function used to turn 90 degrees left
 void turnLeft() {
     motor(0, 35);
     motor(1, 35);
@@ -9,6 +10,7 @@ void turnLeft() {
     msleep(4150);
 } 
 
+//Function used to move forward for a set time
 void forward(int time) {
     motor(0,50);
     motor(1,50);
@@ -17,6 +19,7 @@ void forward(int time) {
     msleep(time);
 }
 
+//Function used to move backward for a set time
 void backward(int time) {
     motor(0,-50);
     motor(1,-50);
@@ -25,6 +28,7 @@ void backward(int time) {
     msleep(time);
 }
 
+//Function used to turn 90 degrees right
 void turnRight() {
     motor(2, 35);
     motor(3, 35);
@@ -34,6 +38,7 @@ void turnRight() {
     msleep(4200);
 }
 
+//Function used to add a break in the robot's motion
 void delay(int time) {
 	motor(2, 0);
     motor(3, 0);
@@ -43,12 +48,14 @@ void delay(int time) {
     msleep(time);
 }
 
+//function used to activate the sorting mechanism on the front of the robot.
 void sortTop() {
     set_servo_position(1, 2047);
     msleep(1000);
     set_servo_position(1, 460);
 }
 
+//function used to pick up the first ball
 void pickUpBall() {
 	set_servo_position(3, 1100);
     delay(700);
@@ -60,12 +67,14 @@ void pickUpBall() {
     delay(700);
 }
 
+//function used to set the robot into its starting position.
 void startingPosition() {
 	set_servo_position(1, 0);
 	set_servo_position(3, 1700);        
 	set_servo_position(0, 700);
 }
 
+//Function used to drop the ball
 void dropBall() {
 	set_servo_position(0, 1400);
     delay(500);
@@ -74,6 +83,7 @@ void dropBall() {
 
 }
 
+//Main logic where code it run
 void start() {
     startingPosition();
     //set_servo_position(1,1066);
